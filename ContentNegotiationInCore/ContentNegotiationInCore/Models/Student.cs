a2052dc8a250300f9e0b3a33f0dce5e264c4a3dd
princeprasad28 <princeprasad.1991@gmail.com>
2020-03-15 21:32:39 +0530

    Commit for Entity Creation and Data Context

---
+++ ContentNegotiationInCore/ContentNegotiationInCore/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContentNegotiationInCore.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        [Required]
+        public string FName { get; set; }
+        [Required]
+        public string LName { get; set; }
+        [Required]
+        public int Age { get; set; }
+        [Required]
+        public long Contact { get; set; }
+        [Required]
+        public long City { get; set; }
+    }
+}
